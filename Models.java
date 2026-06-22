adb953be4ed6e0ac712247b5501176346554dfbe
Hxrsx_s7 <harsas147@gmail.com>
2026-06-22 16:50:24 +0530

    Implement Book class with availability check
    Added Book class with properties and methods for managing book availability.

--- Models.java
+++ Models.java
@@ -1,1 +1,24 @@
-// Will contain Book and Member classes
+class Book {
+    String bookId;
+    String title;
+    String author;
+    String genre;
+    int totalCopies;
+    int availableCopies;
+    int timesBorrowed = 0;
+
+    public Book(String bookId, String title, String author, String genre, int totalCopies) {
+        this.bookId = bookId;
+        this.title = title;
+        this.author = author;
+        this.genre = genre;
+        this.totalCopies = totalCopies;
+        this.availableCopies = totalCopies;
+    }
+
+    public boolean isAvailable() {
+        return availableCopies > 0;
+    }
+}
+
+// Member class coming in next commit// Will contain Book and Member classes
