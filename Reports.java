61b6f3f134b1826c5aab0457c31d0e4c6f6a867a
prethiyangaran <prethidivs0123@gmail.com>
2026-06-29 22:09:00 +0530

    Refactor Reports class to include logging methods

--- Reports.java
+++ Reports.java
@@ -1,19 +1,15 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Map;
-
 public class Reports {
-
     public static List<Book> getLeaderboard(Map<String, Book> books, int topN) {
         List<Book> sorted = new ArrayList<>(books.values());
         sorted.sort((a, b) -> b.timesBorrowed - a.timesBorrowed);
         return sorted.subList(0, Math.min(topN, sorted.size()));
     }
-}
-public static void logAction(MyStack<String> historyStack, String bookId, String action) {
-    historyStack.push(bookId + " - " + action + " - " + java.time.LocalDate.now());
-}
-
-public static List<String> getHistory(MyStack<String> historyStack) {
-    return historyStack.toList();
-}
+    public static void logAction(MyStack<String> historyStack, String bookId, String action) {
+        historyStack.push(bookId + " - " + action + " - " + java.time.LocalDate.now());
+    }
+    public static List<String> getHistory(MyStack<String> historyStack) {
+        return historyStack.toList();
+    } }
